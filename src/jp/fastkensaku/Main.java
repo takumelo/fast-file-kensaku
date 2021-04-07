a0b5bfbdf6b0162507d8cc19d607d7481ba9c1d8
takumelo <midon341@gmail.com>
2021-04-07 23:28:28 +0900

    モック画面にタブとクローズボタンの追加

--- src/jp/fastkensaku/Main.java
+++ src/jp/fastkensaku/Main.java
@@ -1,13 +1,13 @@
 package jp.fastkensaku;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
-    private JFrame frame1;
-    private JTabbedPane tabbedPane1;
-    private JMenuBar menuBar;
-    private JMenu menu, submenu;
-    private JMenuItem menuItem;
+
+    private JTabbedPane tab;
+    JPanel panel;
 
     private JMenu createSettingMenu() {
         JMenu fileMenu = new JMenu("設定");
@@ -32,11 +32,44 @@ public class Main {
         return menuBar;
     }
 
+    private JTabbedPane createTab(){
+        tab = new JTabbedPane();
+        panel = new JPanel();
+        panel.add(createTextField());
+        panel.add(createSearchButton());
+        tab.add("検索", panel);
+        return tab;
+    }
+
+    private JTextField createTextField(){
+        JTextField field = new JTextField(10);
+        return field;
+    }
+
+    private JButton createSearchButton(){
+        JButton button = new JButton("検索");
+        button.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                tab.add("結果", createResultPanel());
+                int ind = tab.getTabCount() - 1;
+                tab.setTabComponentAt(ind, new ButtonTabComponent(tab));
+            }
+        });
+        return button;
+    }
+
+    private JPanel createResultPanel(){
+        JPanel panel = new JPanel();
+        return panel;
+    }
+
+
     private void initGui() {
-        frame1 = new JFrame();
+        JFrame frame1 = new JFrame();
         frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame1.setTitle("高速ファイル検索君");
         frame1.setJMenuBar(createMenuBar());
+        frame1.add(createTab());
         frame1.setSize(400, 350);
         frame1.setLocationByPlatform(true);
         frame1.setVisible(true);
