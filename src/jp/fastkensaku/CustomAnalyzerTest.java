157de61117b664b1288d3f3d5a2c8e5a09c65a0e
takumelo <midon341@gmail.com>
2021-05-07 00:30:12 +0900

    PatternReplaceFilterで空文字列に置換したトークンがそのまま登録されていたバグの修正とテストの追加

---
+++ src/jp/fastkensaku/CustomAnalyzerTest.java
@@ -0,0 +1,64 @@
+package jp.fastkensaku;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.highlight.*;
+import org.junit.Test;
+
+import java.io.StringReader;
+
+public class CustomAnalyzerTest {
+    @Test
+    public void testJapaneseAnalyzer() throws Exception {
+        String text = "日本it企業の遅れ";
+        CustomJapaneseAnalyzer analyzer = new CustomJapaneseAnalyzer();
+        TokenStream tokenStream = analyzer.tokenStream("test", text);
+
+        OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+
+        tokenStream.reset();
+        while (tokenStream.incrementToken()) {
+            int startOffset = offsetAttribute.startOffset();
+            int endOffset = offsetAttribute.endOffset();
+            String term = charTermAttribute.toString();
+            if(term.equals("")){
+                System.out.println("[空白]");
+            }else{
+                System.out.println(term);
+            }
+        }
+    }
+    @Test
+    public void testKanaJapaneseAnalyzer() throws Exception {
+
+    }
+    @Test
+    public void testEnglishAnalyzer() throws Exception {
+
+    }
+    @Test
+    public void testJapaneseHighlight() throws Exception{
+        CustomJapaneseAnalyzer jAnalyzer = new CustomJapaneseAnalyzer();
+        Query jq = new QueryParser("jaContent", jAnalyzer).parse("IT");
+
+        Formatter formatter = new SimpleHTMLFormatter();
+        QueryScorer scorer = new QueryScorer(jq);
+        Highlighter highlighter = new Highlighter(formatter, scorer);
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer, 2);
+        highlighter.setTextFragmenter(fragmenter);
+        TokenStream stream = TokenSources.getTokenStream("jaContent", null, "日本IT企業の勃興", jAnalyzer, -1);
+
+        //Get highlighted text fragments
+        String[] frags = highlighter.getBestFragments(stream, "日本IT企業の勃興", 3);
+        for(String i: frags){
+            System.out.println(i);
+        }
+    }
+}
--- src/jp/fastkensaku/CustomJapaneseAnalyzer.java
+++ src/jp/fastkensaku/CustomJapaneseAnalyzer.java
@@ -3,9 +3,11 @@ package jp.fastkensaku;
 import java.util.regex.Pattern;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.StopFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
+import org.apache.lucene.analysis.ja.JapaneseBaseFormFilter;
 import org.apache.lucene.analysis.ja.JapaneseTokenizer;
 import org.apache.lucene.analysis.ja.dict.UserDictionary;
 import org.apache.lucene.analysis.pattern.PatternReplaceFilter;
@@ -22,10 +24,16 @@ public class CustomJapaneseAnalyzer extends Analyzer {
     @Override
     protected TokenStreamComponents createComponents(String fieldName) {
         JapaneseTokenizer src = new JapaneseTokenizer(jaUserDict, true, JapaneseTokenizer.Mode.NORMAL);
-        // ascii文字のみ
+
+        // ascii文字のみを置換
         Pattern p = Pattern.compile("\\p{ASCII}");
         TokenStream result = new PatternReplaceFilter(src, p, "", true);
-        result = new StopFilter(src,  JapaneseAnalyzer.getDefaultStopSet());
+
+        // 置換した空白トークンと日本語のストップワードの削除
+        CharArraySet stopSet = StopFilter.makeStopSet("");
+        stopSet.add(JapaneseAnalyzer.getDefaultStopSet());
+        result = new StopFilter(result,  stopSet);
+
         return new TokenStreamComponents(src, result);
     }
 }
