184c125be9548d5d533035fadadecf51725151eb
takumelo <midon341@gmail.com>
2021-05-07 00:49:59 +0900

    英語のアナライザも日本語のアナライザと同様にascii以外を置換しようとしたけどうまくいかなかった

--- src/jp/fastkensaku/CustomAnalyzerTest.java
+++ src/jp/fastkensaku/CustomAnalyzerTest.java
@@ -41,7 +41,24 @@ public class CustomAnalyzerTest {
     }
     @Test
     public void testEnglishAnalyzer() throws Exception {
+        String text = "日本it企業の遅れ";
+        CustomEnglishAnalyzer analyzer = new CustomEnglishAnalyzer();
+        TokenStream tokenStream = analyzer.tokenStream("test", text);
 
+        OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
+        CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+
+        tokenStream.reset();
+        while (tokenStream.incrementToken()) {
+            int startOffset = offsetAttribute.startOffset();
+            int endOffset = offsetAttribute.endOffset();
+            String term = charTermAttribute.toString();
+            if(term.equals("")){
+                System.out.println("[空白]");
+            }else{
+                System.out.println(term);
+            }
+        }
     }
     @Test
     public void testJapaneseHighlight() throws Exception{
@@ -72,6 +89,24 @@ public class CustomAnalyzerTest {
         Fragmenter fragmenter = new SimpleSpanFragmenter(scorer, 2);
         highlighter.setTextFragmenter(fragmenter);
         TokenStream stream = TokenSources.getTokenStream("jaKanaContent", null, "日本IT企業の勃興", jAnalyzer, -1);
+
+        //Get highlighted text fragments
+        String[] frags = highlighter.getBestFragments(stream, "日本IT企業の勃興", 3);
+        for(String i: frags){
+            System.out.println(i);
+        }
+    }
+    @Test
+    public void testEnglishHighlight() throws Exception {
+        CustomEnglishAnalyzer eAnalyzer = new CustomEnglishAnalyzer();
+        Query jq = new QueryParser("EngContent", eAnalyzer).parse("日本");
+
+        Formatter formatter = new SimpleHTMLFormatter();
+        QueryScorer scorer = new QueryScorer(jq);
+        Highlighter highlighter = new Highlighter(formatter, scorer);
+        Fragmenter fragmenter = new SimpleSpanFragmenter(scorer, 2);
+        highlighter.setTextFragmenter(fragmenter);
+        TokenStream stream = TokenSources.getTokenStream("EngContent", null, "日本IT企業の勃興", eAnalyzer, -1);
 
         //Get highlighted text fragments
         String[] frags = highlighter.getBestFragments(stream, "日本IT企業の勃興", 3);
--- src/jp/fastkensaku/CustomEnglishAnalyzer.java
+++ src/jp/fastkensaku/CustomEnglishAnalyzer.java
@@ -2,12 +2,10 @@ package jp.fastkensaku;
 
 import java.util.regex.Pattern;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.LowerCaseFilter;
-import org.apache.lucene.analysis.StopFilter;
-import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.*;
 import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.ja.JapaneseAnalyzer;
 import org.apache.lucene.analysis.miscellaneous.CapitalizationFilter;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 import org.apache.lucene.analysis.pattern.PatternReplaceFilter;
@@ -19,11 +17,17 @@ public class CustomEnglishAnalyzer extends Analyzer {
         // https://stackoverflow.com/questions/61075242/is-there-a-way-to-remove-all-special-characters-using-lucene-filters
         // TODO: WhitespaceTokenizer()のほうがいい？
         StandardTokenizer src = new StandardTokenizer();
-        // ascii文字以外
+
+        // ascii文字以外を置換
         Pattern p = Pattern.compile("[^\\p{ASCII}]");
         TokenStream result = new PatternReplaceFilter(src, p, " ", true);
         result = new LowerCaseFilter(src);
-        result = new StopFilter(result,  EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
+
+        // 置換した空白トークンと英語のストップワードの削除
+        CharArraySet stopSet = StopFilter.makeStopSet("");
+        stopSet.add(EnglishAnalyzer.ENGLISH_STOP_WORDS_SET);
+        result = new StopFilter(result,  stopSet);
+
         result = new PorterStemFilter(result);
         result = new CapitalizationFilter(result);
         return new TokenStreamComponents(src, result);
